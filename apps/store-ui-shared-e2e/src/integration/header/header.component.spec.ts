2e289e52e1d56d32ea66c1e5dd6e352978b7734d
Joelle Ortiz <Joelle.Ortiz@Nintex.com>
2020-03-31 08:38:23 +1100

    e2e for title

--- apps/store-ui-shared-e2e/src/integration/header/header.component.spec.ts
+++ apps/store-ui-shared-e2e/src/integration/header/header.component.spec.ts
@@ -1,7 +1,12 @@
 describe('store-ui-shared', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=headercomponent--primary&knob-title='));
+  beforeEach(() =>
+    cy.visit(
+      '/iframe.html?id=headercomponent--primary&knob-title=BoardGameHoard'
+    )
+  );
 
   it('should render the component', () => {
     cy.get('bg-hoard-header').should('exist');
+    cy.get('bg-hoard-header').contains('BoardGameHoard');
   });
 });
