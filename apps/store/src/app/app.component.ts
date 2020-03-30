84faf87e9a733c771e21d1dd38068eef674514dc
Joelle Ortiz <Joelle.Ortiz@Nintex.com>
2020-03-31 08:00:32 +1100

    api in app

--- apps/store/src/app/app.component.ts
+++ apps/store/src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { getAllGames } from '../fake-api';
 
 import { formatRating } from '@bg-hoard/store/util-formatters';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'bg-hoard-root',
@@ -9,8 +10,14 @@ import { formatRating } from '@bg-hoard/store/util-formatters';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(private httpClient: HttpClient) {}
+
   title = 'Board Game Hoard';
-  games = getAllGames().map(game => {
-    return { ...game, rating: formatRating(game.rating) };
-  });
+  games = this.httpClient.get<object[]>('/api/games').pipe(
+    map(games =>
+      games.map(game => {
+        return { ...game, rating: formatRating((game as any).rating) };
+      })
+    )
+  );
 }
--- apps/store/src/app/app.module.ts
+++ apps/store/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
@@ -24,7 +25,8 @@ import { StoreUiSharedModule } from '@bg-hoard/store/ui-shared';
       { initialNavigation: 'enabled' }
     ),
     MatCardModule,
-    StoreUiSharedModule
+    StoreUiSharedModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
